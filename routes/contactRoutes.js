73fed02837c5bcb404bc78e37bdfa3affa7300c9
NoOne <71642636+abdulhannan13300@users.noreply.github.com>
2023-09-11 21:59:53 +0530

    Update contactRoutes.js
    Remove unnecessary comments

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,14 +6,7 @@ const validateToken = require("../middleware/validateTokenHandler");
 router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
-//OR Routes with similar end points are combined above or  
-// you can do it different line as below
 
-// router.route("/").get(getContacts);
-// router.route('/').post(createContact);
-// router.route('/:id').get(getContact);
-// router.route('/:id').put(updateContact);
-// router.route('/:id').delete(deleteContact);
 
 
 module.exports=router;
